a3ffda81ab32f3a6161d30ce7c595ae87381752c
Avinash <avinash.pinnamaneni@lesystems.eu>
2024-01-26 16:52:55 +0100

    process mapping is done through excel on;y

--- customSim/PPR/basicProcessSetup.py
+++ customSim/PPR/basicProcessSetup.py
@@ -1,3 +1,4 @@
+import ast
 import simpy
 import pandas as pd
 import sys 
@@ -15,10 +16,10 @@ domains = get_classes(PPRClasses) # generating the domains which are defined as 
 
 directory_path = '../customSim/LES/systemDefinition'
 
-for domain in domains:
-    domain.object_list = model_domain(env, domain, directory_path) # defines objects based on the attributes defined in the excel sheet
-    # print(f'modelled {domain}')
-    # print(f'The objects of the domain: {domain.__name__} are {domain.object_list}') # prints the objects created while the domains are modelled
+# importing data from excel formatted as {'object_id' : [[],[]]}
+process_flow_model = make_process_flow_model(env, domains, directory_path)
+
+print(process_flow_model)
 
 object_list = Product.object_list + Process.object_list + Resource.object_list 
 
@@ -28,30 +29,12 @@ object_list = Product.object_list + Process.object_list + Resource.object_list
 - Resources such as buffers can be added to peocess flow model, to which the upstream and downstream processes are generated during runtime.
 - Process serves as a pivot connecting resources and products and hence processes are being simulated.
 '''
-
-# definition of process flow model
-process_flow_model = {'proc_1':[['proc_2', 'proc_3', 'proc_4', 'proc_5', 'res_5'], ['proc_6', 'proc_7', 'proc_8', 'proc_9']] } # define the process model as a nested list
+'''
+# process flow model generated from excel as it is the front end
 
 processes = map_processes(process_flow_model, object_list) # generates the upstream and downstream processes based on process flow model
 
 
-# adding mapping data back to the excel sheet
-for domain in domains:
-    if domain.__name__ in ['Process', 'Resource']:
-        file_path = os.path.join(directory_path, f'{domain.__name__}.xlsx')
-        # print(object for object in domain.object_list)
-        for object in domain.object_list:
-            upstream_proc = []
-            downstream_proc = []
-            print(object.upstream_processes)
-'''
-            for process in object.upstream_processes:
-                upstream_proc.append(process.name)
-            for process in object.downstream_processes:
-                downstream_proc.append(process.name)
-            
-            add_data_to_excel(file_path, 'id', object.id, 'upstream_processes', str(upstream_proc))
-            add_data_to_excel(file_path, 'id', object.id, 'downstream_processes', str(downstream_proc))
 
 
 def simulate_factory(env, processes, orders):
--- customSim/PPR/functions.py
+++ customSim/PPR/functions.py
@@ -1,22 +1,19 @@
-# Importing packages
 import ast
 import simpy
 import pandas as pd
 import inspect
 import os
-
-
-## Importing the path of current working directory
 import sys
-sys.path.insert(1, 'H:/My Drive/Thesis/Simulation/customSim') ## importing the path of current working directory
 
+# Importing the path of the current working directory
+sys.path.insert(1, 'H:/My Drive/Thesis/Simulation/customSim')
 
 from PPR import PPRClasses
 from PPR.PPRClasses import *
 from PPR.Functions import *
 
-# function to add attributes to an object during run time
 def add_attribute(self, attribute):
+    """Add attributes to an object during runtime."""
     if not isinstance(attribute, dict):
         raise ValueError("Attribute should be a dictionary")
 
@@ -25,45 +22,44 @@ def add_attribute(self, attribute):
         self.attributes[key] = value 
 
 
-def get_classes(library_module):  # function which returns the list of classes available in the module
+def get_classes(library_module):
+    """Return a list of classes available in the module."""
     classes = []
     for name, obj in inspect.getmembers(library_module):
         if inspect.isclass(obj):
             classes.append(obj)
-
     return classes
 
 
 def get_attributes(class_type):
-        attributes = class_type().attributes
-        return attributes
+    """Get attributes of a class type."""
+    attributes = class_type().attributes
+    return attributes
 
 
 def evaluate_cost(object):
-    # to evaluate the cost based on the list of components being used in case of assemblies and products
+    """Evaluate the cost based on the list of components being used in case of assemblies and products."""
     pass
 
 
 def model_domain(env, domain, directory_path):
+    """Model a domain and create instances of the object."""
     object_list = []
-    file_path = f'{directory_path}/{domain.__name__}.xlsx' # file path of excel sheet for the given class
-    attribute_list = domain(env).attributes # list of atributes of a class
+    file_path = f'{directory_path}/{domain.__name__}.xlsx'
+    attribute_list = domain(env).attributes
     
-    if os.path.isfile(file_path):  # if sheet exists at the given path
-        class_df = pd.read_excel(file_path, usecols=lambda x: 'Unnamed' not in x)  # import excel sheet as a pandas dataframe
+    if os.path.isfile(file_path):
+        class_df = pd.read_excel(file_path, usecols=lambda x: 'Unnamed' not in x)
 
-        with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:  # Open Excel writer outside the loop
-            for attribute in attribute_list:  # iterating through attributes of the class and add missing columns corresponding to attributes
+        with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+            for attribute in attribute_list:
                 if attribute not in class_df.columns:
-                    new_column = pd.Series(name=attribute, dtype=object)  # Create a new column with the required name
-                    class_df = pd.concat((class_df, new_column), axis=1)  # adding attributes as an index in the dataframe
+                    new_column = pd.Series(name=attribute, dtype=object)
+                    class_df = pd.concat((class_df, new_column), axis=1)
 
-            class_df.to_excel(writer, sheet_name=f'{domain.__name__}', index=True)  # Write the updated dataframe to the Excel file
+            class_df.to_excel(writer, sheet_name=f'{domain.__name__}', index=True)
 
-        
-        # Creating instances of the object
         for index, row in class_df.iterrows():
-            # Create an instance of the class dynamically based on class name
             class_instance = domain(env)  
             for col_name, value in row.items():
                 if col_name != 'kwargs':
@@ -80,76 +76,95 @@ def model_domain(env, domain, directory_path):
             object_list.append(class_instance)
            
     else:
-        new_df = pd.DataFrame({})  # create an empty pandas dataframe
-        
-        for attribute in attribute_list:
-                new_df[f'{attribute}'] = [] # adding attributes as index in the dataframe
+        new_df = pd.DataFrame({})
 
-        with pd.ExcelWriter(file_path, engine='openpyxl') as writer: # Replacing the excel sheet with updated dataframe 
+        for attribute in attribute_list:
+            new_df[f'{attribute}'] = []
+
+        with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
             new_df.to_excel(writer, sheet_name=f'{domain.__name__}', index=True)
 
         print(f'Added the sheet: {domain.__name__}. Please update the sheet.')
-
     return object_list
 
 
-def map_processes(process_flow_model, object_list): # generates upstream and downstream processes for each of the processes
+def map_processes(process_flow_model, object_list):
+    """Generate upstream and downstream processes for each process."""
     processes = []
-    for process_id, network in process_flow_model.items():
-        input_processes  = network[0]
-        output_processes  = network[1]
-        upstream_processes = define_upstream(process_id, input_processes, object_list)
-        downstream_processes = define_downstream(process_id, output_processes, object_list)
-        
-        # adding current process to the list
-        current_process = get_process_object(process_id, object_list)
-        if current_process not in processes:
-                processes.append(current_process)
 
+    for process_id, network in process_flow_model.items():
+        input_processes = network[0]
+        output_processes = network[1]
+        
+        print(get_name_list(get_process_object(process_id, object_list).upstream_processes))
+        upstream_processes = define_upstream(process_id, input_processes, object_list)
+        print(get_name_list(get_process_object(process_id, object_list).upstream_processes))
+        downstream_processes = define_downstream(process_id, output_processes, object_list)
+
+        current_process = get_process_object(process_id, object_list)
+        
+        # Check if the current process is not already in the list before appending
+        if current_process not in processes:
+            processes.append(current_process)
+
+    # Add upstream and downstream processes to the list
     processes = processes + upstream_processes + downstream_processes
 
     for process in processes:
         if process is not None:
+            # Get lists of names for upstream and downstream processes
             upstream_processes = get_name_list(process.upstream_processes)
             downstream_processes = get_name_list(process.downstream_processes)
-            print(f'{process.name}: {upstream_processes}, {downstream_processes}')
+            # print(f'{process.name}: {upstream_processes}, {downstream_processes}')
+
         else:
             print("No production objects defined.")
 
-        
-     
-
+# Function to get a list of names from a list of objects or strings
 def get_name_list(list_of_objects):
+    """Return a list of names from a list of objects or strings."""
     name_list = []
-    for object in  list_of_objects:
-        name_list.append(object.name)
+    for obj in list_of_objects:
+        if hasattr(obj, 'name'):
+            name_list.append(obj.name)
+        else:
+            name_list.append(str(obj))  # Convert non-object elements to string
     return name_list
 
+
 def get_process_object(process_id, object_list):
-    return next((obj for obj in object_list if obj.id == process_id), None) # returns process object matching the id passed
+    """Return the process object matching the given id."""
+    return next((obj for obj in object_list if obj.id == process_id), None)
 
 
 def define_upstream(process_id, input_processes, object_list):
+
     process_list = []
     current_process = get_process_object(process_id, object_list)
+    
     if current_process is not None:
 
         for input_process_id in input_processes:
             input_process_object = get_process_object(input_process_id, object_list)
 
-            # if input_process_object is not None:
-            if input_process_object not in current_process.upstream_processes:
-                current_process.upstream_processes.append(input_process_object)
+            if input_process_object is not None:
+                if input_process_object not in current_process.upstream_processes:
+                    current_process.upstream_processes.append(input_process_object)
 
-            if current_process not in input_process_object.downstream_processes:
-                input_process_object.downstream_processes.append(current_process)
+                if current_process not in input_process_object.downstream_processes:
+                    input_process_object.downstream_processes.append(current_process)
 
-            process_list.append(input_process_object)
+
+                process_list.append(input_process_object)
+            else:
+                print(f"Warning: Process with ID {input_process_id} not found in object_list")
+
+
     return process_list
 
-       
 
 def define_downstream(process_id, output_processes, object_list):
+
     process_list = []
     current_process = get_process_object(process_id, object_list)
 
@@ -168,89 +183,25 @@ def define_downstream(process_id, output_processes, object_list):
                 process_list.append(output_process_object)
             else:
                 print(f"Warning: Process with ID {output_process_id} not found in object_list")
-
+    
     return process_list
 
+def make_process_flow_model(env, domains, directory_path):
+    process_flow_model = {} 
+    # generating process flow model from
+    for domain in domains:
+        domain.object_list = model_domain(env, domain, directory_path) # defines objects based on the attributes defined in the excel sheet
+        if domain.__name__ in ['Process', 'Resource']:
+            for object in domain.object_list:
+                upstream_process_list = []
+                downstream_process_list = []
+                for process in object.upstream_processes:
+                    upstream_process_list.append(process)
+
+                for process in object.downstream_processes:
+                    downstream_process_list.append(process)
 
 
-def add_data_to_excel(file_path, search_column, search_value, target_column, data_to_add):
-    try:
-        # Read the Excel file
-        df = pd.read_excel(file_path, usecols=lambda x: 'Unnamed' not in x)
-
-        # Find the index of the row based on the value of the search_column
-        index_to_update = df.index[df[search_column] == search_value].tolist()
-
-        # Check if the row was found
-        if index_to_update:
-            # Get the existing list as a string from the specified cell
-            existing_list_str = df.at[index_to_update[0], target_column]
-
-            # Convert the string to a list using ast.literal_eval
-            existing_list = ast.literal_eval(existing_list_str)
-
-            # Update the list (add new elements, remove duplicates, etc.)
-            for element in data_to_add:
-                if element not in existing_list:
-                    existing_list.append(element)
-
-            # Convert the updated list back to a string
-            updated_list_str = str(existing_list)
-
-            # Add the updated string to the target_column in the selected row
-            df.at[index_to_update[0], target_column] = updated_list_str
-
-            # Write the updated DataFrame back to the Excel file
-            with pd.ExcelWriter(file_path, engine='openpyxl', mode='a') as writer:
-                df.to_excel(writer, index=False)
-
-            print(f"Data added successfully for {search_column}={search_value}.")
-        else:
-            print(f"Row not found for {search_column}={search_value}.")
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-
-
-
-
-'''
------------- Function for making contianer ------------
-def make_container(env, product):
-    return simpy.Container(env, capacity = 5, init = 5)
-    pass
-    
------------- Function for addding resource to a process ------------
-## processObject is the object to which the list is being updated and objectsList is the list of objects of lower hierarchical level
-def update_resources(processObject, objectsList): # Updates the resources of the object bsaed on the objects of lower hierarchical level
-    if isinstance(objectsList, list):
-        for process in objectsList:
-            processObject.resources.update(process.resources)
-    elif not isinstance(objectsList, list) and not isinstance(objectsList, tuple) and not isinstance(objectsList, dict) and not isinstance(objectsList, set):
-        processObject.resources.update(process.resources)
-    else:
-        raise TypeError("Invalid datatype for 'processes'. It should be a list or a single object.")
-
------------- Function for adding supplies to an object ------------
-def update_supplies(processObject, objectsList):
-    if isinstance(objectsList, list):
-        for process in objectsList:
-            processObject.supplies.update(process.resources)
-    elif not isinstance(objectsList, list) and not isinstance(objectsList, tuple) and not isinstance(objectsList, dict) and not isinstance(objectsList, set):
-        processObject.supplies.update(process.resources)
-    else:
-        raise TypeError("Invalid datatype for 'skills'. It should be a list or a single object.")
-
------------- Function for adding skills to an object ------------
-## Updating the skills based on the objects of lower hierarchy
-def update_skills(processObject, objectsList):
-    if isinstance(objectsList, list):
-        for process in objectsList:
-            processObject.skills.update(process.resources)
-    elif not isinstance(objectsList, list) and not isinstance(objectsList, tuple) and not isinstance(objectsList, dict) and not isinstance(objectsList, set):
-        processObject.skills.update(process.resources)
-    else:
-        raise TypeError("Invalid datatype for 'skills'. It should be a list or a single object.")
-'''
+                process_map = {str(object.name) : [upstream_process_list, downstream_process_list]}
+                process_flow_model.update(process_map)
+    return process_flow_model
